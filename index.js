6cb999fbfc89a1d473fec8f118877fa83afba2b5
Lucas Mofardini <lucasmofardini@gmail.com>
2022-11-20 02:39:24 -0300

    Criando com pk e sk

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ let fs = require('fs');
 let path = require('path')
 let csvToJson = require('convert-csv-to-json');
 
-let fileInputName = `./csv/${process.argv[2]}`; 
-let fileOutputName = `./json/${process.argv[2]}`;
+// node index.js TipoCultivo.csv SK
+
+let nameFile = process.argv[2];
+let fileInputName = `./csv/${nameFile}`; 
+let fileOutputName = `./json/${nameFile}`;
+
+const SK = process.argv[3];
 
 csvToJson.utf8Encoding().getJsonFromCsv(fileInputName);
 
@@ -18,27 +23,26 @@ const treatingFields = (obj) => {
         let newProperty = lib.translation(property);
         let newValue = lib.cleanValue(obj[property])
 
+        if(newProperty == 'Code') {
+            newObj['SK'] = SK;
+            newObj['PK'] = `${SK}-${newValue}`;
+        }
+    
         newObj[newProperty] = newValue;
     }
 
     return newObj
 }
-
-json.map((item) => {
-    
-    return {item}
-})
-
 json = json.map(element => {
-    // Object.getOwnPropertyNames(element)
-    // console.log(element)
-   return final = treatingFields(element);
+   let newJson = treatingFields(element);
+   console.log(newJson)    
+   return newJson; 
 
 });
 
 json = JSON.stringify(json);
 
-console.log(json);
+// console.log(json);
 
 
 fs.writeFile(fileOutputName ,json, (error)=>{
@@ -46,6 +50,4 @@ fs.writeFile(fileOutputName ,json, (error)=>{
 
     console.log('Foi criado com sucesso');
 })
-
-// csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
 
